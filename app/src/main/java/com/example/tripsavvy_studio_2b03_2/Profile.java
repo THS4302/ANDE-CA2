484891eef2d75e79eadf9c0c0a95638233381d23
THS4302 <THETHSAN.22@ichat.sp.edu.sg>
2024-01-13 23:54:55 +0800

    profile

--- app/src/main/java/com/example/tripsavvy_studio_2b03_2/Profile.java
+++ app/src/main/java/com/example/tripsavvy_studio_2b03_2/Profile.java
@@ -9,7 +9,7 @@ public class Profile extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourites);
+        setContentView(R.layout.activity_profile);
 
     }
 }
